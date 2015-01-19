780d9e79aad9969d44b47562dd1362f3861029ff
13744695 <HelenaRothwell@gmail.com>
2015-01-19 22:57:04 +0000

    Updates
    Updated code to allow for greater than 180 degrees and converts to only
    print the smaller angle between the hands.

--- Clock.java
+++ Clock.java
@@ -1,26 +1,26 @@
-/*Write a Java method that takes in the current time in hours and
-minutes and outputs the angle in degrees between the hour and
-minute hands on a watch displaying the current time (e.g. at
-midnight the angle is 0 degrees).
-*/
-public class Clock
-{
-	public static void main(String []args)
-{
+/*Write a Java method that takes in the current time in hours and minutes and outputs the angle
+ *in degrees between the hour and minute hands on a watch displaying the current time (e.g. at
+ *midnight the angle is 0 degrees).   */
+public class Clock{
+	public static void main(String []args){
+	int hour = 4;
+	int min = 15;
+	int hourinmins = hour*5;
+	System.out.println("hourinmins is 5 * hours and is: " +hourinmins);
 
 
-int hour =12;
-int min = 15;
+	int diff = hourinmins-min;
+	System.out.println("The difference in minutes between the hour and minute hands is: "+diff);
 
-int diff = hour-min;
-System.out.println(diff);
-
-if (diff<0)
-diff=-diff;
-System.out.println("positive of diff is "+diff);
-
-int angle = 3*diff;
-System.out.println("angle " +angle);
-
-}
+	if (diff<0){
+		diff=hourinmins-diff;
+		System.out.println("positive of diff is "+diff);
+				}
+	if (diff>30){
+		hourinmins = hourinmins-30;
+	System.out.println("adjusted hoursinmin is: "+hourinmins);
+				}
+	int angle = 3*diff;
+	System.out.println("angle is: " +angle +" degrees");
+	}
 }
